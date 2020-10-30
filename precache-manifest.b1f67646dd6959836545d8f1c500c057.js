e8d062e2f84580851e05d7076c85832b7d399555
GoutamSamal9 <gsamal101@gmail.com>
2020-10-30 20:54:37 +0530

    Updates

---
+++ precache-manifest.b1f67646dd6959836545d8f1c500c057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bca8b594108c7c136422c7a67845212",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "094afd022c1b2860b45e",
+    "url": "/amazon-clone/static/css/main.ca252b17.chunk.css"
+  },
+  {
+    "revision": "2e8de582b31bf44bdaa2",
+    "url": "/amazon-clone/static/js/2.826e3c6e.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.826e3c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094afd022c1b2860b45e",
+    "url": "/amazon-clone/static/js/main.bf8a7c86.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
